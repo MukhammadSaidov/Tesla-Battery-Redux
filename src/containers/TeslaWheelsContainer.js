f37f3bdfd18a1303296d206b41f1b4fda4e335cf
MukhammadSaidov <77840212+MukhammadSaidov@users.noreply.github.com>
2021-09-19 09:02:11 +0500

    Finally Tesla Battery

---
+++ src/containers/TeslaWheelsContainer.js
@@ -0,0 +1,20 @@
+import TeslaWheels from "../components/TeslaWheels/TeslaWheels";
+import { connect } from "react-redux";
+import { changeWheel } from "../action/index";
+
+const mapStateToProps = (state) => {
+    return {
+        value: state.config.wheels
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        changeHandler: (size) => {
+            dispatch(changeWheel(size))
+        }
+    }
+}
+
+const TeslaWheelsContainer = connect(mapStateToProps, mapDispatchToProps)(TeslaWheels)
+export default TeslaWheelsContainer
